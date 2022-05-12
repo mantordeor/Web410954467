4f2e3761f4be79dcf657f48b6a4e3b70c4f4ee8e
賴秉閎 <weeeeeeeeeeeeeeeeeeeeeeee>
2022-05-12 20:51:10 +0800

    add signup

---
+++ Web410954467/Web410954467/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410954467.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(string name, string account, string password)
+        {
+            if(name == null || name == "")
+            {
+                ViewBag.NameMessage = "請輸入姓名";
+            }
+            if (account == null || account == "")
+            {
+                ViewBag.AccountMessage = "請輸入帳號";
+            }
+            if (password == null || password == "")
+            {
+                ViewBag.PasswordMessage = "請輸入密碼";
+            }
+            if (name != "" && account != "" && password != "")
+            {
+                ViewBag.Message = "註冊成功";
+            }
+            return View();
+        }
+    }
+}
