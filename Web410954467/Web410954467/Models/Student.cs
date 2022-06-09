a096254d903b5d6f88a9dc5330c56e5083a31203
賴秉閎 <weeeeeeeeeeeeeeeeeeeeeeee>
2022-06-09 18:42:04 +0800

    add IsActive

--- Web410954467/Web410954467/Models/Student.cs
+++ Web410954467/Web410954467/Models/Student.cs
@@ -18,5 +18,6 @@ namespace Web410954467.Models
         public string Name { get; set; }
         public string Number { get; set; }
         public string Email { get; set; }
+        public Nullable<bool> IsActive { get; set; }
     }
 }
